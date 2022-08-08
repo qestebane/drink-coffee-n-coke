acb29bd2afae3049cffbd4b5e15819f009603743
qestebane <elisabelaq.com>
2022-08-08 01:18:02 -0500

    print message

---
+++ firstCode.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println("Happy Birthday, Eli")
+     // Let's print a cake!
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+
+    // This prints an empty line.
+    println("")
+    println("You are already 2x!")
+    println("2x is the very best age to celebrate!")
+}
